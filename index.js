86d3c0a04538cc97ffc4e8005bc319d91eb0ae70
Rakibul-Islam01 <iamrakibulislam01@gmail.com>
2023-05-27 22:52:59 +0600

    delete functionality added

--- index.js
+++ index.js
@@ -87,6 +87,15 @@ async function run() {
     })
 
 
+    // delete a car toy
+    app.delete('/toys/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await toysCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
 
 
     // Send a ping to confirm a successful connection
