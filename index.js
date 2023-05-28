674a9229a923cd4401e1b7a479f8c34c6f351a7f
Rakibul-Islam01 <iamrakibulislam01@gmail.com>
2023-05-28 07:26:54 +0600

    something update

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors());
-// app.options("", cors(corsConfig))
 app.use(express.json());
+// app.options("", cors(corsConfig))
 
 
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_password}@cluster0.03ostmu.mongodb.net/?retryWrites=true&w=majority`;
-// const uri = "mongodb+srv://car-toys:THqqFxGj3tQ1W41N@cluster0.03ostmu.mongodb.net/?retryWrites=true&w=majority";
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,11 +31,9 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const toysCollection = client.db("carToys").collection("toys");
-    // const db = client.db("carToys");
-    // const toysCollection = db.collection("toys");
 
     app.get('/toys', async(req, res)=>{
         const result = await toysCollection.find({}).toArray()
